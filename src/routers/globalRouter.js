7214ed8b458270874dcb8772740a906c4247f0e5
Kelly <>
2021-11-12 15:41:39 +0900

    router

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import join from "../controllers/userController"
-import trending from "../controllers/videoController"
+import { join, login } from "../controllers/userController"
+import { trending, search } from "../controllers/videoController"
 
 // All these files we are creating are known as modules, and they are isolated.
 // Every Router is bubble.
@@ -8,6 +8,8 @@ const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 // default export
 export default globalRouter;
