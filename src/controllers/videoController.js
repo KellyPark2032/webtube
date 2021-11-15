04d175a94fe99f52fa85654f320bb13a26a079e5
박경은 <pke9696@gmail.com>
2021-11-15 22:44:23 +0900

    iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,37 @@
 // multiple export
-export const trending = (req, res) => 
-    // use variables
-    res.render("home", {pageTitle: "Home"});
-export const search = (req, res) => res.send("Search");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: 'First Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes age',
+      id: 1,
+    },
+    {
+      title: 'Second Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes age',
+      id: 1,
+    },
+    {
+      title: 'Third Video',
+      rating: 5,
+      comments: 2,
+      createdAt: '2 minutes age',
+      id: 1,
+    },
+  ]
+  // use variables
+  return res.render('home', { pageTitle: 'Home', videos })
+}
+export const search = (req, res) => res.send('Search')
 
 // request object has informations about request
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
-export const edit = (req, res) => res.render("Edit", {pageTitle: "Edit"});
+export const see = (req, res) => res.render('watch', { pageTitle: 'Watch' })
+export const edit = (req, res) => res.render('Edit', { pageTitle: 'Edit' })
 export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
+  return res.send('Delete Video')
 }
-export const upload = (req, res) => res.send("Upload");
+export const upload = (req, res) => res.send('Upload')
--- src/server.js
+++ src/server.js
@@ -1,51 +1,50 @@
-import express from "express";
-import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
+import express from 'express'
+import morgan from 'morgan'
+import globalRouter from './routers/globalRouter'
+import userRouter from './routers/userRouter'
+import videoRouter from './routers/videoRouter'
 
-const PORT = 4000;
+const PORT = 4000
 
 // find Current Working Directory
 // Current Working Directory is that starting Node.js
-console.log(process.cwd());
+console.log(process.cwd())
 
 // create express application called "app"
-const app = express();
+const app = express()
 
 // setting express to use view engine
 // when send file to pug, pug change this file to HTML
-app.set("view engine", "pug");
+app.set('view engine', 'pug')
 
 // Middleware doesn't respond to the request.
 // we will use morgan.
-const logger = morgan("dev");
+const logger = morgan('dev')
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
+app.set('view engine', 'pug')
+app.set('views', process.cwd() + '/src/views')
 
-app.use(logger);
+app.use(logger)
 
 // configure our application about how to "get" request
 // GET = one of the HTTP METHOD
-app.use('/', globalRouter);
-app.use("/videos", videoRouter);
-app.use('/users', userRouter);
+app.use('/', globalRouter)
+app.use('/videos', videoRouter)
+app.use('/users', userRouter)
 // app.get("/", home);
 // app.get("/login", login);
 
 // route handler has two objects => req, res (+ next)
 // handler = controller, and all controllers have middleware.
 const home = (req, res) => {
-    console.log("I will respond.")
-    res.send("hello");
+  console.log('I will respond.')
+  res.send('hello')
 }
 const login = (req, res) => {
-    return res.send("login");
+  return res.send('login')
 }
 
-const handleListening = () => 
-    console.log(`✅ Server listening on port ${PORT}`);
+const handleListening = () => console.log(`✅ Server listening on port ${PORT}`)
 
 // we have to make listen application
-app.listen(PORT, handleListening); 
+app.listen(PORT, handleListening)
