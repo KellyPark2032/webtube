e2b9e178ac7f9b7786c23d1b0f8d6a3d597f5dea
박경은 <pke9696@gmail.com>
2021-11-17 21:46:40 +0900

    use fake database

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { join, login } from "../controllers/userController"
-import { trending, search } from "../controllers/videoController"
+import { join, login } from "../controllers/userController";
+import { trending } from "../controllers/videoController";
 
 // All these files we are creating are known as modules, and they are isolated.
 // Every Router is bubble.
@@ -9,7 +9,6 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 // default export
 export default globalRouter;
